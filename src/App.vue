7a3550c654c1d24e0e493ce52d9d50b0d005b7d9
DacD <dacdao01@gmail.com>
2019-08-22 19:15:54 -0400

    added some new feature

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,200 @@
 <template>
-
   <div id="app">
+    <h1 style="text-align: center; margin-top:150px; margin-bottom: 80px">Quality of Life</h1>
+    <div>
+      <div>
+        <b-container class="bv-example-row">
+          <b-row>
+            <b-col>
+              <b-form-group>
+                <template slot="label">
+                  <br>
+                  <b-form-checkbox
+                    style
+                    v-model="allSelected"
+                    aria-describedby="flavours"
+                    aria-controls="flavours"
+                    @change="toggleAll"
+                  >{{ allSelected ? 'Un-select All' : 'Select All' }}</b-form-checkbox>
+                </template>
 
-<div style="max-width: 500px; margin: auto">
-  <b-form-group>
-    <template slot="label">
+                <b-form-checkbox-group
+                  id="flavors"
+                  size="lg"
+                  v-model="selected"
+                  :options="Automation"
+                  name="flavors"
+                  class="ml-5"
+                  aria-label="Individual flavours"
+                  stacked
+                ></b-form-checkbox-group>
+              </b-form-group>
+            </b-col>
 
-<h1 style="text-align: center">Quality of Life</h1>
+            <b-col>
+              <div>
+                <progress-bar
+                  style="margin-top: 58px; margin-bottom: 10px; width: 95%"
+                  size="tiny"
+                  text-position="inside"
+                  :val="increasing_pct_Ecom"
+                  :text="increasing_pct_Ecom == 100 ? completedPerc:increasing_pct_Ecom + '%' "
+                />
 
-      <br>
-      <b-form-checkbox
-        v-model="allSelected"
+                <progress-bar
+                  style="margin-bottom: 10px; width: 95%"
+                  size="tiny"
+                  text-position="inside"
+                  :val="increasing_pct_Real"
+                  :text="increasing_pct_Real == 100 ? completedPerc:increasing_pct_Real+ '%' "
+                />
 
-        aria-describedby="flavours"
-        aria-controls="flavours"
-        @change="toggleAll"
-      >
-        {{ allSelected ? 'Un-select All' : 'Select All' }}
-      </b-form-checkbox>
-    </template>
+                <progress-bar
+                  style="margin-bottom: 10px; width: 95%"
+                  size="tiny"
+                  text-position="inside"
+                  :val="increasing_pct_PnA"
+                  :text="increasing_pct_PnA == 100 ? completedPerc:increasing_pct_PnA+ '%' "
+                />
 
-    <b-form-checkbox-group
-      id="flavors"
-      v-model="selected"
-      :options="flavours"
-      name="flavors"
-      class="ml-4"
-      aria-label="Individual flavours"
-      stacked
-    ></b-form-checkbox-group>
-  </b-form-group>
-
-  <div>
-    Selected: <strong>{{ selected }}</strong><br>
-
-  </div>
-</div>
-
+                <progress-bar
+                  size="tiny"
+                  style="width: 95%"
+                  text-position="inside"
+                  :val="increasing_pct_Vertex"
+                  :text="increasing_pct_Vertex == 100 ? completedPerc:increasing_pct_Vertex+ '%' "
+                />
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
     </div>
-
-
-
-
+    <br>
+    <br>
+    <b-button
+      variant="primary"
+      style="margin-left: 400px; width:27%"
+      size="lg"
+      @click="toStart()"
+    >Run Test</b-button>
+    <b-button
+      variant="danger"
+      style="margin-left: 55px; width: 27%"
+      size="lg"
+      v-on:click="toStop()"
+    >Stop Test</b-button>
+  </div>
 </template>
 
 <script>
-
+import ProgressBar from "vue-simple-progress";
+import { setInterval } from "timers";
+import { clearInterval } from "timers";
 
 export default {
-  el:"#app",
-    data() {
-      return {
-        flavours: [
-          'Orange',
-          'Grape',
-          'Apple',
-          'Lime'],
-        selected: [],
-        allSelected: false,
+  name: "app",
+  data() {
+    return {
+      Automation: [
+        "ECommerce SRT",
+        "Real Time Pricing (RTP)",
+        "Pricing and Availability (PnA)",
+        "Vertex"
+      ],
+      selected: [],
+      allSelected: false,
+      random_pct: 0,
+      increasing_pct_Ecom: 0,
+      increasing_pct_Real: 0,
+      increasing_pct_PnA: 0,
+      increasing_pct_Vertex: 0,
+      vertex_cond: true,
+      completedPerc: "passed",
+      firsttime: true
+    };
+  },
+  components: {
+    ProgressBar
+  },
+  methods: {
+    toStart(event) {
+      if (this.selected === undefined || this.selected.length === 0) {
+        alert("please select an option");
+      }
 
+      var vertexNum = this.selected.includes("Vertex");
+      if (vertexNum === true) {
+        this.timerD = setInterval(() => {
+          this.increasing_pct_Vertex = Math.min(
+            this.increasing_pct_Vertex + Math.floor(Math.random() * 50 + 1),
+            100
+          );
+          if (this.counter) {
+            clearInterval(this.timerB);
+          }
+        }, 2000);
+      }
+
+      var EcomNum = this.selected.includes("ECommerce SRT");
+      if (EcomNum === true) {
+        this.timerC = setInterval(() => {
+          this.increasing_pct_Ecom = Math.min(
+            this.increasing_pct_Ecom + Math.floor(Math.random() * 50 + 1),
+            100
+          );
+        }, 2000);
+      }
+
+      var RealNum = this.selected.includes("Real Time Pricing (RTP)");
+      if (RealNum === true) {
+        this.timerB = setInterval(() => {
+          this.increasing_pct_Real = Math.min(
+            this.increasing_pct_Real + Math.floor(Math.random() * 50 + 1),
+            100
+          );
+        }, 2000);
+      }
+
+      var PNAnum = this.selected.includes("Pricing and Availability (PnA)");
+      if (PNAnum === true) {
+        this.timerA = setInterval(() => {
+          this.increasing_pct_PnA = Math.min(
+            this.increasing_pct_PnA + Math.floor(Math.random() * 50 + 1),
+            100
+          );
+        }, 2000);
+      }
+
+      if (this.firsttime) {
+        return (this.firsttime = false);
+      } else {
+        location.reload();
       }
     },
-    methods: {
-      toggleAll(checked) {
-        this.selected = checked ? this.flavours.slice() : []
-      }
+    toStop(event) {
+      clearInterval(this.timerA);
+      clearInterval(this.timerB);
+      clearInterval(this.timerC);
+      clearInterval(this.timerD);
     },
-    watch: {
-      selected(newVal, oldVal) {
-        // Handle changes in individual flavour checkboxes
-        if (newVal.length === 0) {
-          this.indeterminate = false
-          this.allSelected = false
-        } else if (newVal.length === this.flavours.length) {
-          this.indeterminate = false
-          this.allSelected = true
-        } else {
-          this.indeterminate = true
-          this.allSelected = false
-        }
+    toggleAll(checked) {
+      this.selected = checked ? this.Automation.slice() : [];
+    }
+  },
+  watch: {
+    selected(newVal, oldVal) {
+      if (newVal.length === 0) {
+        this.allSelected = false;
+      } else if (newVal.length === this.Automation.length) {
+        this.allSelected = true;
+      } else {
+        this.allSelected = false;
       }
     }
   }
+};
 </script>
 
 <style >
-
-body{
-
-}
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import ProgressBar from "vue-simple-progress";
 
-Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(ProgressBar);
+
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
